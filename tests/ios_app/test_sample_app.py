8ac4e69371166fd5457a37d0a509e3ec767fa73f
Daniil Zverev <svarhik348@gmail.com>
2023-11-10 03:58:14 +0300

    update commit

--- tests/ios_app/test_sample_app.py
+++ tests/ios_app/test_sample_app.py
@@ -9,7 +9,7 @@ from tests.conftest import ios
 def test_text_input():
     with allure.step('Type text'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Text Button')).click()
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Text Input')).type('hello@browserstack.com').press_enter()
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Text Input')).type('Hello world!').press_enter()
 
     with allure.step('Verify typed text'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Text Output')).should(have.text('hello@browserstack.com'))
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Text Output')).should(have.text('Hello world!'))
